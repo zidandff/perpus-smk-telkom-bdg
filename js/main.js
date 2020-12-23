6b800245caa8aaaec849b1b7b0fe0d5b0bd7a99c
Zidan M. Daffa <zidandaff@gmail.com>
2020-12-23 19:26:31 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,48 @@
+// navbar on scroll
+const navbar = document.querySelector('.navbar')
+const headline = document.querySelector('.headline')
+window.addEventListener('scroll', ()=> {
+    if(pageYOffset > headline.offsetTop - 80){
+        navbar.style.backgroundColor = "#fff"
+        navbar.style.transition = ".3s"
+        navbar.style.boxShadow = "0 0px 15px rgba(0, 0, 0, 0.1)"
+    }else {
+        navbar.style.backgroundColor = "transparent"
+        navbar.style.transition = ".3s"
+        navbar.style.boxShadow = "0 0px 15px rgba(0, 0, 0, 0)"
+    }
+})
+
+const navToggler = document.querySelector('.navbar-toggler')
+navToggler.addEventListener('click', ()=> {
+    navbar.style.backgroundColor = "#fff"
+    navbar.style.transition = ".3s"
+    navbar.style.boxShadow = "0 0px 15px rgba(0, 0, 0, 0.1)"
+})
+
+
+// Searchbar animation
+const searchBar = document.querySelector('.input-group');
+const inputbar = document.querySelector('input.form-control')
+
+inputbar.addEventListener('focus', function(){
+    searchBar.style.transform = "translate(4px, 4px)";
+    searchBar.style.boxShadow = "0 0 black";
+})
+
+inputbar.addEventListener('focusout', function(){
+    searchBar.style.transform = "translate(0, 0)";
+    searchBar.style.boxShadow = "5px 5px #CF1B1B";
+})
+
+// light Gallery library
+lightGallery(document.getElementById('lightbox'),{
+    selector: '.item'
+}); 
+
+
+// AOS library
+AOS.init({
+    offset: 170,
+    duration: 800
+});
